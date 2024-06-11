4aef61d088f38185e21b4491f53fe01d1acbf77e
Imran Ahmad <imranjmi463@gmail.com>
2024-06-11 18:41:22 +0530

    move functions into separate file

--- app/src/main/java/com/example/shoppinglist/Home.kt
+++ app/src/main/java/com/example/shoppinglist/Home.kt
@@ -1,33 +1,16 @@
 package com.example.shoppinglist
 
-import android.Manifest
-import android.content.Context
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,11 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-
 
 @Composable
 fun ShoppingListApp() {
@@ -144,77 +124,5 @@ fun ShoppingListApp() {
 
                 }
             })
-    }
-}
-
-@Composable
-fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit) {
-    var editedName by remember { mutableStateOf(item.name) }
-    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
-    var isEditing by remember { mutableStateOf(item.isEditing) }
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-            .background(Color.White),
-        horizontalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Column {
-            BasicTextField(
-                value = editedName,
-                onValueChange = { editedName = it },
-                singleLine = true,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(8.dp)
-            )
-            BasicTextField(
-                value = editedQuantity,
-                onValueChange = { editedQuantity = it },
-                singleLine = true,
-                modifier = Modifier
-                    .wrapContentSize()
-                    .padding(8.dp)
-            )
-
-        }
-
-        Button(onClick = {
-            isEditing = false
-            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
-        }) {
-            Text(text = "Save")
-        }
-
-    }
-}
-
-@Composable
-fun ShoppingListItems(
-    items: ShoppingItem, onEditClick: () -> Unit, onDeleteClick: () -> Unit
-) {
-    Row(
-        modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth()
-            .border(
-                border = BorderStroke(2.dp, Color(0XFF018786)), shape = RoundedCornerShape(20)
-            ), horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-
-        Text(text = items.name, modifier = Modifier.padding(8.dp))
-        Text(text = "Qty: ${items.quantity}", modifier = Modifier.padding(8.dp))
-
-        Row(modifier = Modifier.padding(8.dp)) {
-
-            IconButton(onClick = { onEditClick() }) {
-                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
-            }
-
-            IconButton(onClick = { onDeleteClick() }) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
-            }
-        }
     }
 }
